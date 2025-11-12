3473f2eb2c93db74772f8b3987f1e30b864e4a7b
cidavidson001 <cidavidson001@converse.edu>
2025-11-12 12:00:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 const querystring = require("querystring");
 
 const PORT = 3000;
 
+// Define absolute path to public directory
+const publicDir = path.join(__dirname, "public");
+
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === "/") {
-    // Serve hw5-work.html
-    fs.readFile("hw5-work.html", "utf8", (err, data) => {
+    // Serve hw5-work.html from /public
+    const filePath = path.join(publicDir, "hw5-work.html");
+
+    fs.readFile(filePath, "utf8", (err, data) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/plain" });
         res.end("Error reading hw5-work.html");
@@ -21,16 +27,14 @@ const server = http.createServer((req, res) => {
   else if (req.method === "POST" && req.url === "/") {
     let body = "";
 
-    // Collect POST data
     req.on("data", chunk => {
       body += chunk.toString();
     });
 
     req.on("end", () => {
-      // Parse any kind of form data (URL-encoded)
       const formData = querystring.parse(body);
 
-      // Build an HTML table of all received data
+      // Build table of *all* received data
       let tableRows = "";
       if (Object.keys(formData).length > 0) {
         for (const [key, value] of Object.entries(formData)) {
@@ -49,18 +53,20 @@ const server = http.createServer((req, res) => {
         <br>
       `;
 
-      // Read hw5-work.html to reuse form and CSS
-      fs.readFile("hw5-work.html", "utf8", (err, htmlData) => {
+      // Read hw5-work.html from /public
+      const filePath = path.join(publicDir, "hw5-work.html");
+
+      fs.readFile(filePath, "utf8", (err, htmlData) => {
         if (err) {
           res.writeHead(500, { "Content-Type": "text/plain" });
           res.end("Error reading hw5-work.html");
           return;
         }
 
-        // Remove "Homework 5" header (case-insensitive)
+        // Remove "Homework 5" header if present
         const modifiedHtml = htmlData.replace(/<h1[^>]*>.*?Homework 5.*?<\/h1>/i, "");
 
-        // Insert the data table before the form
+        // Insert table before the form
         const finalHtml = modifiedHtml.replace(
           /(<form[^>]*>)/i,
           `${tableHTML}$1`
@@ -70,3 +76,15 @@ const server = http.createServer((req, res) => {
         res.end(finalHtml);
       });
     });
+  }
+
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
